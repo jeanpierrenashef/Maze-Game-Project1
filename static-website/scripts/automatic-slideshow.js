47fced74f4c2eb4f7805fe47e61e112ee5accd8e
unknown <jeanpierrenashef@gmail.com>
2024-10-29 05:04:59 +0300

    Incorporated slideshow in tutorials section

---
+++ static-website/scripts/automatic-slideshow.js
@@ -0,0 +1,15 @@
+const total_slides = document.querySelectorAll('.slide').length; 
+const slides = document.querySelector('.slides');
+
+let index = 0; 
+
+
+function slide_navigation() {
+    slides.style.marginLeft = `-${index * 800}px`;
+    index++;
+    if (index >= total_slides) {
+        index = 0; 
+    }
+}
+
+setInterval(slide_navigation, 2000);
