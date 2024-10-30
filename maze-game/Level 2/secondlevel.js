5663034a507b49d1b8f2feda79b9c11daf282803
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-10-31 01:29:57 +0200

    uploading winning in level 2

--- maze-game/Level 2/secondlevel.js
+++ maze-game/Level 2/secondlevel.js
@@ -32,20 +32,22 @@ let moveDurations = [];
 let timers = [];
 let speeds = 140; 
 let startTime;
+let winStatus=false;
 
 
 function preload() {
     this.load.image('player', 'https://cdn-icons-png.flaticon.com/128/742/742751.png');
-    this.load.image('bot', 'https://cdn-icons-png.flaticon.com/512/595/595582.png'); // Replace with your player image
+    this.load.image('bot', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJHzqyMhVvW_nyQoFPUheldQ0ZOnZiwGOBqw&s'); // Replace with your player image
+    this.load.image('diamond', 'https://bluemoji.io/cdn-proxy/646218c67da47160c64a84d5/66b3e9ea16121c4a0759ffbb_53.png'); // Replace with your player image
      // Replace with your player image
     this.load.image('coin', 'https://static.vecteezy.com/system/resources/thumbnails/023/588/193/small/coin-with-dollar-sign-golden-dollar-symbol-gold-coin-3d-stack-of-gold-coins-icon-isolated-symbol-png.png'); // Replace with your coin image
 }
 //bot = this.physics.add.sprite(50, 500, 'bot').setOrigin(10, 10).setScale(0.15);
 //bot.setCollideWorldBounds(true);
 
-function create() {
-    this.cameras.main.setBackgroundColor('#190321');     
-    this.cameras.main.setZoom(0.5); // Zoom out to show more of the game world
+function create(){
+    this.cameras.main.setBackgroundColor('#091538');     
+    this.cameras.main.setZoom(0.8); // Zoom out to show more of the game world
     
     // Initialize player, coins, bots, etc.
     player = this.physics.add.sprite(50, 50, 'player').setScale(0.15);
@@ -85,7 +87,7 @@ function create() {
         ' WWWWWWWWWWWW  WWWWWWWWWW  WWWWWWWWWWW',
         ' W  WWWWWWWWWWWWWWW  WWWWWWWWWWWWWWWWW',
         ' WWWWWwWW  WWWWWWWWWWWWWWWWW  WWWWWWWW',
-        ' WWWWWWWWWWWWWWWWW  WWWWWWWWWWWWWWWWWW',
+        ' WWWWWWWWWWWWWWWWW N  WWWWWWWWWWWWWWWW',
         
     ];
     maze3 = [' ',' ',' ',' ',
@@ -118,16 +120,19 @@ function create() {
         'IWWWWWWWWWWWW  WWWWWWWWW  WWWWWWWWWWW I',
         'IWW  WWWWWWWWWWWWW  WWWWWWWWWWWWWWWWW I',
         'IWWWWWWWW  WWWWWWWWWWWWWWWW  WWWWW  W I',
-        'IWWWWWWWWWWWWWWWW  WWWWWWWWWWWWWWWWWWI',
+        'IWWWWWWWWWWWWWWWWNNNWWWWWWWWWWWWWWWWWWI',
         
     ];
-    this.cameras.main.setBackgroundColor('#190321');
     var coinPositions = [];
     let walls = [];
     let beams = [];
     var array=[];
     var arrayT=[];
+   
+
     let boxes = [];
+   
+
     
     
     for (let i = 0; i < maze3.length; i++) {
@@ -147,7 +152,14 @@ function create() {
     for (let i = 0; i < maze.length; i++) {
         for (let j = 0; j < maze[i].length; j++) {
             
-        
+        /*if(maze[i][j]==='N'){
+            
+            const x = j*20;
+                const y = i*50;
+                arrayG.push({x,y});
+                console.log(x,y); 
+
+        }*/
             if (maze[i][j] === 'W') {
                 const boundary = this.add.text(j * 20, i * 50, '—', {
                     fontSize: '40px',
@@ -179,14 +191,22 @@ function create() {
                 }).setOrigin(0,0).setScale(0.5);
                 
                 this.physics.add.existing(box, true);
-                boxes.push(box);}
+            boxes.push(box);}
         }}
         coinPositions=array;
         botPositions=arrayT;
+        
 
         coins = this.physics.add.group();
         bots = this.physics.add.group();
+        
 
+        
+            const gate = this.physics.add.sprite(400, 820, 'diamond');
+            gate.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+            gate.setScale(0.06);
+
+    
         botPositions.forEach(p => {
             const bot = bots.create(p.x, p.y, 'bot');
             bot.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
@@ -209,10 +229,40 @@ function create() {
 
     this.physics.add.collider(player, walls);
     this.physics.add.collider(bots, walls);
+    this.physics.add.overlap(player, gate,()=>{
+        const winningtext = this.add.text( 1000,  300, 'Congratulations, you WIN', {
+            fontSize: '40px',
+            fill: '#05f5f5',
+            fontFamily: 'Arial'
+        }).setScale(0.5).setInteractive();
+        winningtext.setVisible(true);
+        const winningtext2 = this.add.text( 1000,  400, 'You can return to the main menu', {
+            fontSize: '40px',
+            fill: '#05f5f5',
+            fontFamily: 'Arial'
+        }).setScale(0.5).setInteractive();
+        winningtext2.setVisible(true);
+        const returnbtn = this.add.text( 1000,  500, 'Main Menu', {
+            fontSize: '40px',
+            fill: '#05f5f5',
+            fontFamily: 'Arial'
+        }).setScale(0.5).setInteractive();
+        returnbtn.setVisible(true);
+        const reloadbtn = this.add.text( 1200,  500, 'Replay', {
+            fontSize: '40px',
+            fill: '#05f5f5',
+            fontFamily: 'Arial'
+        }).setScale(0.5).setInteractive();
+        reloadbtn.setVisible(true);
+        this.scene.pause();
+        
+    },null,this);
+
 
     this.physics.add.collider(player, beams)
 
-    this.physics.add.collider(player, bots, this.hitBomb, null, this);
+
+    
 
 
 
@@ -229,7 +279,19 @@ function create() {
     
     scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#fff' });
     cursors = this.input.keyboard.createCursorKeys();
+
+}
+
+function loose(){
+    console.log('loose');
+}
+function win(scene){
+    return true;
     
+    winStatus=true;
+    console.log(winStatus);
+    //scene.winningtext.setVisible(true);
+
 }
 
 function update() {
